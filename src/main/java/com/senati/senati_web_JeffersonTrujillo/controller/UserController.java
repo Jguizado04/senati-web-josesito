38cc5bc622ca3b64d9f1c3a43351baa1cb857130
Ramses Capillo <1594608@senati.pe>
2025-10-16 08:57:11 -0500

    mensajes

--- src/main/java/com/senati/senati_web_JeffersonTrujillo/controller/UserController.java
+++ src/main/java/com/senati/senati_web_JeffersonTrujillo/controller/UserController.java
@@ -3,7 +3,10 @@ package com.senati.senati_web_JeffersonTrujillo.controller;
 import com.senati.senati_web_JeffersonTrujillo.model.User;
 import com.senati.senati_web_JeffersonTrujillo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,4 +20,9 @@ public class UserController {
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
+
+
+    @PostMapping("/api/user")
+    public ResponseEntity<User> savaUser (@RequestBody User user) { return userService.SaveUser(user); }
+    
 }
--- src/main/java/com/senati/senati_web_JeffersonTrujillo/service/UserService.java
+++ src/main/java/com/senati/senati_web_JeffersonTrujillo/service/UserService.java
@@ -3,7 +3,10 @@ package com.senati.senati_web_JeffersonTrujillo.service;
 import com.senati.senati_web_JeffersonTrujillo.model.User;
 import com.senati.senati_web_JeffersonTrujillo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -15,4 +18,14 @@ public class UserService {
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
+
+    public ResponseEntity<User> SaveUser(@RequestBody User user){
+        return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
+
+    }
+
+
+
+
+
 }
